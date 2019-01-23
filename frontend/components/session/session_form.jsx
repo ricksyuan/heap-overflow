c1ed8153884b81f01aa54407f65cb0e1412ea9ea
Rick Yuan <rick.s.yuan@gmail.com>
2019-01-23 14:10:55 -0500

    Fix additional errors with session form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -37,7 +37,6 @@ export default class SessionForm extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <div className="session">
         <form className="session-form" onSubmit={this.handleSubmit}>
           <label>Display Name</label>
@@ -45,53 +44,28 @@ export default class SessionForm extends React.Component {
             className="name"
             type="text"
             placeholder="J. Doe"
-=======
-      <div className='session'>
-        <form className='session-form' onSubmit={this.handleSubmit}>
-          <label>Display Name</label>
-          <input
-            className='name'
-            type='text'
-            placeholder='J. Doe'
->>>>>>> a22841df667f58e82a03d91dc255bec5792ba468
             value={this.state.email}
             onChange={this.handleChange}
           />
 
           <label>Email (required, but never shown)</label>
           <input
-<<<<<<< HEAD
             className="text"
             type="text"
             placeholder="you@example.org"
-=======
-            className='text'
-            type='text'
-            placeholder='you@example.org'
->>>>>>> a22841df667f58e82a03d91dc255bec5792ba468
             value={this.state.email}
             onChange={this.handleChange}
           />
           <label>Password</label>
           <input
-<<<<<<< HEAD
             className="password"
             type="password"
             placeholder="********"
-=======
-            className='password'
-            type='password'
-            placeholder='********'
->>>>>>> a22841df667f58e82a03d91dc255bec5792ba468
             value={this.state.password}
             onChange={this.handleChange}
           />
           <input
-<<<<<<< HEAD
             type="submit"
-=======
-            type='submit'
->>>>>>> a22841df667f58e82a03d91dc255bec5792ba468
             value={this.props.type}
           />
         </form>
