ae4443330278eb91acd5b9b1e80c57a6f80b93e6
Rick Yuan <rick.s.yuan@gmail.com>
2019-01-28 16:43:20 -0500

    Switch between root and log in/signup

--- frontend/components/header/logo.jsx
+++ frontend/components/header/logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <>
-      <img className="header-logo-img" src={window.headerLogo} />
-    </>
+      <Link className="header-logo-link" to="/">
+        <img className="header-logo-img" src={window.headerLogo} />
+      </Link>
   );
 };
 
--- frontend/components/main/content.jsx
+++ frontend/components/main/content.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import LoginContainer from '../session/login_container';
 import SignupContainer from '../session/signup_container';
 import { AuthRoute } from '../../utils/route_util';
-import { Route } from 'react-router';
+import { Switch, Route } from 'react-router';
 import TopQuestions from '../questions/top_questions';
 const Content = () => {
   return (
     <div className="content">
-      <Route to="/" component={TopQuestions} />
-      <div className="session-page">
-        <AuthRoute exact path="/login" component={LoginContainer} />
-        <AuthRoute exact path="/signup" component={SignupContainer} />
-      </div>
+        <Route exact path="/" component={TopQuestions} />
+        <div className="session-page">
+          <AuthRoute exact path="/login" component={LoginContainer} />
+          <AuthRoute exact path="/signup" component={SignupContainer} />
+        </div>
+
     </div>
   );
 };
