636621c69c422df4ddfa6eb24c4e3c032c5e29cd
Rick Yuan <rick.s.yuan@gmail.com>
2019-01-28 10:03:28 -0500

    Remove unnecessary migration changes

--- db/migrate/20190125211629_fix_accepted.rb
+++ db/migrate/20190125211629_fix_accepted.rb
@@ -1,7 +1,4 @@
 class FixAccepted < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :answers, :accepted
-    remove_column :answers, :boolean
-    add_column :answers, :accepted, :boolean, null: false, default: false
+  def change    
   end
 end
