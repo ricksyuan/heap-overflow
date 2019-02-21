073a2bbd8b461dc5e2b95e8d6ee875eafeceea96
Rick Yuan <rick.s.yuan@gmail.com>
2019-02-20 19:20:36 -0500

    Reset primary key sequences after seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,3 +106,7 @@ CSV.foreach(File.join(__dir__, 'seed_post_tags.csv'), headers: true) do |post_ta
   tag_id = post_tag_row['TagId'];
   Tagging.create!(question_id: question_id, tag_id: tag_id);
 end
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
